5368d6a66cfa3efae751d147e41d97e4fc8f3803
Yusuf Büyüktaş <buyuktasyusuf688@gmail.com>
2025-10-15 02:51:18 +0300

    Controller katmanı bitti

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using web_odev.Models;
 
+
 namespace web_odev.Controllers;
 
 public class HomeController : Controller
@@ -15,8 +16,57 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        var students = Repository.Students;
+        List<Student> signedStudents = new List<Student>();
+
+        foreach (Student student in Repository.Students)
+        {
+            if (student.Signed)
+            {
+                signedStudents.Add(student);
+            }
+
+        }
+
+        ViewBag.SignedStudents = signedStudents;
         return View();
     }
+
+    public IActionResult AllStudents()
+    {
+        var students = Repository.Students;
+        ViewBag.allstudents = students;
+        return View();
+
+    }
+
+    [HttpGet]
+    public IActionResult Sign()
+    {
+        return View();
+
+    }
+
+    [HttpPost]
+    public IActionResult Sign(int studentID)
+    {
+        Student? student = Repository.Students.FirstOrDefault(s => s.StudentID == studentID);
+        if (student != null)
+        {
+            student.Signed = true;
+            student.SignedAt = DateTime.Now;
+
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            ViewBag.ErrorMessage = "Öğrenci numarası bulunamadı.";
+            return View("Sign");   
+        }
+        
+
+    }
+
 
     public IActionResult Privacy()
     {
